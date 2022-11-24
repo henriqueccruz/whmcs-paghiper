867c7c69342d7f35eb342156e768925a19aff4bf
Henrique Cruz <eu@henriquecruz.com.br>
2022-11-24 15:09:52 -0300

    Module requirements

---
+++ modules/gateways/paghiper/inc/helpers/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '1ea9db8d776669fac33d5688af985a9b98d3600f',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1ea9db8d776669fac33d5688af985a9b98d3600f',
+    ),
+    'nikic/php-parser' => 
+    array (
+      'pretty_version' => 'v4.15.1',
+      'version' => '4.15.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0ef6c55a3f47f89d7a374e6f835197a0b5fcf900',
+    ),
+  ),
+);
