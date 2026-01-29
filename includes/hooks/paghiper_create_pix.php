dfacc2eee8ab0a629636e6796b76bf57020d4c8e
Henrique Cruz <eu@henriquecruz.com.br>
2026-01-29 13:46:21 -0300

    Hooks renamed pt. II

--- includes/hooks/paghiper_create_pix.php
+++ includes/hooks/paghiper_create_pix.php
@@ -1,19 +1,19 @@
 <?php
 /**
- * Adiciona boleto bancário e link direto para boleto no WHMCS
+ * Adiciona QR code do PIX nos templates do WHMCS
  * 
  * @package    PagHiper para WHMCS
  * @version    2.5.3
  * @author     Equipe PagHiper https://github.com/paghiper/whmcs
  * @author     Henrique Cruz
  * @license    BSD License (3-clause)
- * @copyright  (c) 2017-2025, PagHiper
+ * @copyright  (c) 2017-2026, PagHiper
  * @link       https://www.paghiper.com/
  */
 
 if (!defined("WHMCS")) die("This file cannot be accessed directly");
 
-function paghiper_display_digitable_line($vars) {
+function paghiper_display_pix_qr_code($vars) {
 	
 	// PHP 5.x compatibility
 	if (version_compare(PHP_VERSION, '7.0.0') >= 0) {
@@ -30,27 +30,27 @@ function paghiper_display_digitable_line($vars) {
 
     if(in_array($email_template, $target_templates)) {
 
-        $whmcs_url = rtrim(\App::getSystemUrl(),"/");
-
         require_once($basedir . '/modules/gateways/paghiper/classes/PaghiperTransaction.php');
         $paghiperTransaction    = new PaghiperTransaction(['invoiceID' => $invoice_id, 'format' => 'array']);
         $invoiceTransaction     = $paghiperTransaction->process();
 
         if($invoiceTransaction) {
-
-            $digitable_line             = $invoiceTransaction['digitable_line'];
-            $bar_code_number_to_image   = $invoiceTransaction['bar_code_number_to_image'];
-            
+		
+            $digitable_line 	= $invoiceTransaction['emv'];
+            $qrcode_image_url 	= $invoiceTransaction['qrcode_image_url'];
+    
             if($digitable_line) {
-                $merge_fields['linha_digitavel'] = '<div style="text-align: center;" class="billet-barcode-container"><span>Linha digitável: <br><span style="font-size: 16px; color: #000000"><strong>';
-                $merge_fields['linha_digitavel'] .= "<img class='billet-barcode' style='max-width: 100%;' height='50' src='{$whmcs_url}/modules/gateways/paghiper/assets/php/barcode.php?codigo={$bar_code_number_to_image}'><br>";
-                $merge_fields['linha_digitavel'] .= $digitable_line;
-                $merge_fields['linha_digitavel'] .= '</strong></span></span></div>';
+                $merge_fields['codigo_pix'] = "<div style='text-align: center;' class='qr-code-container'><img class='qr-code' width='320' height='320' src='{$qrcode_image_url}'><br>";
+                $merge_fields['codigo_pix'] .= '<h2 style="font-size: 16px; color: #000000">Use a opção QR Code no seu app de internet banking<br><span style="font-size: 14px; font-weight: normal;">Ou, se preferir, copie o texto abaixo para fazer o pagamento</span></h2>';
+                $merge_fields['codigo_pix'] .= '<span>Seu código PIX: <br><span style="font-size: 16px; color: #000000"><strong>';
+                $merge_fields['codigo_pix'] .= $digitable_line;
+                $merge_fields['codigo_pix'] .= '</strong></span></span></div>';
             }
         }
+
 
     }
     return $merge_fields;
 }
 
-add_hook('EmailPreSend', 1, "paghiper_display_digitable_line");
+add_hook('EmailPreSend', 1, "paghiper_display_pix_qr_code");
